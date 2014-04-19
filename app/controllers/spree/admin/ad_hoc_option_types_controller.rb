90dc4c54b1369a3c326c53cf147dbb33d3182abb
johncox00 <john@mach19.com>
2014-04-19 06:35:20 -0600

    trying to debug

--- app/controllers/spree/admin/ad_hoc_option_types_controller.rb
+++ app/controllers/spree/admin/ad_hoc_option_types_controller.rb
@@ -30,7 +30,7 @@ module Spree
 
     private
     def load_product
-      @product = Product.find_by_param!(params[:product_id])
+      @product = Product.with_deleted.friendly.find(params[:product_id])#Product.find_by_param!(params[:product_id])
     end
 
     def load_available_option_values
